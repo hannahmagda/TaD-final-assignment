f2d989b2da012544fc1ca38a0025c131a52358e6
Hannahmagda <hannahschweren@gmail.com>
2023-11-30 18:46:25 +0100

    add oil price plot

--- oilprices_data.R
+++ oilprices_data.R
@@ -13,7 +13,7 @@ oil_price_xml <- read_xml("basketDayArchives.xml")
 # Define the namespace
 ns <- xml_ns(oil_price_xml)
 
-basket_lists <- xml_find_all(doc, ".//*[local-name()='BasketList']", ns)
+basket_lists <- xml_find_all(oil_price_xml, ".//*[local-name()='BasketList']", ns)
 data <- xml_attr(basket_lists, "data")
 val <- xml_attr(basket_lists, "val")
 oil_prices_df <- data.frame(date = data, value = as.numeric(val))
@@ -22,11 +22,13 @@ oil_prices_df$date <- as.Date(oil_prices_df$date)
 oil_prices_df$year <- format(oil_prices_df$date, "%Y")
 
 
-ggplot(oil_prices_df, aes(x = date, y = value)) +
+oil_prices_plot <- ggplot(oil_prices_df, aes(x = date, y = value)) +
   geom_line() +
   labs(title = "Oil Prices Over Time",
        x = "Year",
        y = "Price ($)") +
   theme_minimal()
+
+ggsave("oil_prices.png", plot = oil_prices_plot, width = 7, height = 5, units = "in")
 
 
